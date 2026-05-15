3e5e6edd6e2e087372dffb3add47b128bcc07b7a
Wellington Ferreira de Lima Jr <157297409+WellDevLima@users.noreply.github.com>
2026-05-15 11:44:26 -0300

    Fix SQL syntax for UUID insertion in inserirPokemon

--- src/main/java/com/example/pokedex/model/PokemonDAO.java
+++ src/main/java/com/example/pokedex/model/PokemonDAO.java
@@ -27,7 +27,7 @@ public class PokemonDAO {
     
     public void inserirPokemon(Pokemon pokemon) {
     String sql = "INSERT INTO pokemon (nome, tipo1, descricao, nome_arquivo_foto, usuario_id) " +
-                 "VALUES (?, ?, ?, ?, CAST(? AS UUID))";
+                 "VALUES (?, ?, ?, ?, ?::uuid)";
     jdbcTemplate.update(
         sql,
         pokemon.getNome(),
