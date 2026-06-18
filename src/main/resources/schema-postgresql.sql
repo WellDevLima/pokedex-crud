98a609aac202d8011433e1ad027eeb3622d98350
Wellington Ferreira de Lima Jr <157297409+WellDevLima@users.noreply.github.com>
2026-06-17 23:39:32 -0300

    Modify pokemon table structure
    Added tipo2 column to pokemon table and removed nome_arquivo_foto column.

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -1,18 +1,18 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
+ 
 CREATE TABLE IF NOT EXISTS users (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   username VARCHAR(50) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
+ 
 CREATE TABLE IF NOT EXISTS pokemon (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   nome VARCHAR(100) NOT NULL,
   tipo1 VARCHAR(50),
+  tipo2 VARCHAR(50),
   descricao TEXT,
-  nome_arquivo_foto VARCHAR(255) NOT NULL,
   usuario_id UUID NOT NULL REFERENCES users(id),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
