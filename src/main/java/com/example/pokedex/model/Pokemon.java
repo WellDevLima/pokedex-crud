2af916ff385047a6a64d57378bc3319c8a48020f
Wellington Ferreira de Lima Jr <157297409+WellDevLima@users.noreply.github.com>
2026-06-17 23:35:41 -0300

    Refactor Pokemon class to include tipo2 and remove nomeArquivoFoto

--- src/main/java/com/example/pokedex/model/Pokemon.java
+++ src/main/java/com/example/pokedex/model/Pokemon.java
@@ -7,28 +7,28 @@ public class Pokemon {
     private String id;
     private String nome;
     private String tipo1;
+    private String tipo2;
     private String descricao;
-    private String nomeArquivoFoto;
     private String usuarioId;
     
     public Pokemon() {
     }
     
-    public Pokemon(String nome, String tipo1, String descricao, String nomeArquivoFoto, String usuarioId) {
+    public Pokemon(String nome, String tipo1, String tipo2, String descricao, String usuarioId) {
         this.id = UUID.randomUUID().toString();
         this.nome = nome;
         this.tipo1 = tipo1;
+        this.tipo2 = tipo2;
         this.descricao = descricao;
-        this.nomeArquivoFoto = nomeArquivoFoto;
         this.usuarioId = usuarioId;
     }
     
-    public Pokemon(String id, String nome, String tipo1, String descricao, String nomeArquivoFoto, String usuarioId) {
+    public Pokemon(String id, String nome, String tipo1, String tipo2, String descricao, String usuarioId) {
         this.id = id;
         this.nome = nome;
         this.tipo1 = tipo1;
+        this.tipo2 = tipo2;
         this.descricao = descricao;
-        this.nomeArquivoFoto = nomeArquivoFoto;
         this.usuarioId = usuarioId;
     }
     
@@ -56,20 +56,20 @@ public class Pokemon {
         this.tipo1 = tipo1;
     }
     
+    public String getTipo2() {
+        return tipo2;
+    }
+    
+    public void setTipo2(String tipo2) {
+        this.tipo2 = tipo2;
+    }
+    
     public String getDescricao() {
         return descricao;
     }
     
     public void setDescricao(String descricao) {
         this.descricao = descricao;
-    }
-    
-    public String getNomeArquivoFoto() {
-        return nomeArquivoFoto;
-    }
-    
-    public void setNomeArquivoFoto(String nomeArquivoFoto) {
-        this.nomeArquivoFoto = nomeArquivoFoto;
     }
     
     public String getUsuarioId() {
